c475f72dce801f0b1814a30f34743cb66f2e5855
Teesha6 <70149768+Teesha6@users.noreply.github.com>
2021-04-25 15:55:03 -0400

    Add files via upload

---
+++ TS2-master/TS2-master/js/Block.js
@@ -0,0 +1,52 @@
+class Block{
+    constructor(x, y, width, height) {
+        var options = {
+           
+            restitution :0.4,
+            friction :0.0,
+            //isStatic:true
+           
+        }
+        this.visibility = 225;
+        this.body = Bodies.rectangle(x, y, width, height, options);
+        this.width = width;
+        this.height = height;
+        World.add(world, this.body);
+        
+      }
+     
+      score(){
+        if(this.visibility<8 && this.visibility>=105){
+          score++
+        }
+        block1.score();
+        block2.score();
+        block3.score();
+        block4.score();
+        block5.score();
+        block6.score();
+        block7.score();
+        block8.score();
+        block9.score();
+      }
+
+      display(){
+        console.log(this.body.speed);
+        if(this.body.speed <3){
+        var angle = this.body.angle;
+        var pos= this.body.position;
+        push();
+        translate(pos.x, pos.y);
+        rotate(angle);
+        rectMode(CENTER);
+        rect(0,0,this.width, this.height);
+        pop();
+      }else{
+        World.remove(world, this.body);
+        push();
+        this.visibility = this.visibility -5;
+        pop();
+        
+      }
+    }
+}
